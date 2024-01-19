933324f004bc62e7446fde87e22b1a3cbca1dfd7
Anthony Shellman <atshellman@gmail.com>
2024-01-19 15:57:14 -0700

    Initial commit

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,11 @@
+import '/src/components/SearchBar/SearchBar.scss'
+
+const SearchBar = () => {
+  return (
+    <section className='searchbar'>
+      <p>Searchbar Placeholder</p>
+    </section>
+  )
+}
+
+export default SearchBar

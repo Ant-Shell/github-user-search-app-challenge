eef918ff0ed85704593dd757a8c2aefab1e9cd38
Anthony Shellman <atshellman@gmail.com>
2024-01-19 13:52:45 -0700

    Initial commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+// import Moon from '/src/assets/icon-moon.svg'
+// import Sun from '/src/assets/icon-sun.svg'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header-logo">devfinder</h1>
+      <h2 className="header-dark-toggle">dark</h2>
+    </header>
+  )
+}
+
+export default Header

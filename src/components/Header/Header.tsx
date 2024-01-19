3756e281507808ec719b241ebb36b5639b15ac3f
Anthony Shellman <atshellman@gmail.com>
2024-01-19 15:08:30 -0700

    Add import and initial structuring

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,15 @@
-// import Moon from '/src/assets/icon-moon.svg'
+import Moon from '/src/assets/icon-moon.svg'
 // import Sun from '/src/assets/icon-sun.svg'
+import '/src/components/Header/Header.scss'
 
 const Header = () => {
   return (
     <header className="header">
       <h1 className="header-logo">devfinder</h1>
-      <h2 className="header-dark-toggle">dark</h2>
+      <div className='header-toggle-container'>
+        <h2 className="header-dark-toggle">dark</h2>
+        <img className='header-moon-image' src={Moon} alt='Moon'/>
+      </div>
     </header>
   )
 }

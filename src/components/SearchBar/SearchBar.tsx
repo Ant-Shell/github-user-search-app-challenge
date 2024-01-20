e6cc36905dd5e11da61dc147f2deabbbd9e5438b
Anthony Shellman <atshellman@gmail.com>
2024-01-20 15:01:05 -0700

    Add image, input and button

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,9 +1,17 @@
 import '/src/components/SearchBar/SearchBar.scss'
+import searchIcon from '/src/assets/icon-search.svg'
 
 const SearchBar = () => {
   return (
     <section className='searchbar'>
-      <p>Searchbar Placeholder</p>
+      <img className='searchbar-search-icon' src={searchIcon}/>
+      <input type='search'
+      className='searchbar-search'
+      placeholder='Search GitHub username...'
+      >
+
+      </input>
+      <button>Search</button>
     </section>
   )
 }

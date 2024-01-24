00dd70ba710cc4e39a5a1032ef9836ce00e9888e
Anthony Shellman <atshellman@gmail.com>
2024-01-23 19:57:42 -0700

    Add interface and getUser function, pass prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
-// import { useState } from "react";
-// import { fetchUser } from './utilities/fetchCalls' // use this onclick of search button for search bar
+import { useState } from "react";
+import fetchUser from './utilities/fetchCalls'
 import Header from "./components/Header/Header"
 import SearchBar from "./components/SearchBar/SearchBar"
 import '/src/styles/_global.scss'
 
-// interface User {
-//   avatar_url: string;
-//   bio: string;
-//   blog: string;
-//   company: string;
-//   created_at: string;
-//   email: string;
-//   followers: number;
-//   following: number;
-//   location: string;
-//   name: string;
-//   public_repos: number;
-//   twitter_username: string;
-// }
+interface User {
+  avatar_url: string;
+  bio: string;
+  blog: string;
+  company: string;
+  created_at: string;
+  email: string;
+  followers: number;
+  following: number;
+  location: string;
+  name: string;
+  public_repos: number;
+  twitter_username: string;
+}
 
+const initialState: User = {
+  avatar_url: '',
+  bio: '',
+  blog: '',
+  company: '',
+  created_at: '',
+  email: '',
+  followers: 0,
+  following: 0,
+  location: '',
+  name: '',
+  public_repos: 0,
+  twitter_username: ''
+}
 
 const App = () => {
-  // const [user, setUser] = useState<User>()
-  // const [error, setError] = useState<unknown | string>('')
+  const [user, setUser] = useState<User | unknown>(initialState)
+  const [error, setError] = useState<string | unknown>('')
+
+  const getUser = (username:string):void => {
+    fetchUser(username, setUser, setError)
+  }
 
   return (
     <>
       <Header/>
-      <SearchBar/>
+      <SearchBar getUser={getUser}/>
     </>
   )
 }

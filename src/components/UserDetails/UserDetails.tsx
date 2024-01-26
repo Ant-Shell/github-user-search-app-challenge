a0112d4d502d5845a9cc7812e3ae0d61f90857f1
Anthony Shellman <atshellman@gmail.com>
2024-01-25 18:56:26 -0700

    Initial commit

---
+++ src/components/UserDetails/UserDetails.tsx
@@ -0,0 +1,11 @@
+import '/src/components/UserDetails/UserDetails.scss'
+
+const UserDetails = () => {
+  return (
+    <section className='user-details'>
+      <p>UserDetails Placeholder</p>
+    </section>
+  )
+}
+
+export default UserDetails
